e170e01b6af93e238e712f84585284e27e30101a
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2025-06-15 22:04:48 +0300

    feat: grayscale using ImageSharp library, setup of hierarchy of directories

---
+++ src/Convolution.Cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using Convolution.Main;
+
+public class Program
+{
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Input image path")]
+        public required string InputFile { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output image path")]
+        public required string OutputFile { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args).WithParsed(RunOptions);
+    }
+
+    static void RunOptions(Options opts)
+    {
+        try
+        {
+            var processor = new ImageProcessor();
+            processor.ApplyGrayscale(opts.InputFile, opts.OutputFile);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ src/Convolution.Main/ImageProcessor.cs
@@ -0,0 +1,18 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Convolution.Main;
+
+public class ImageProcessor
+{
+    public void ApplyGrayscale(string inputPath, string outputPath)
+    {
+        using var image = Image.Load(inputPath);
+
+        image.Mutate(x => x.Grayscale());
+
+        image.Save(outputPath);
+        Console.WriteLine($"Image saved to {outputPath}");
+    }
+}
